73e8ff18b0aa26b89e4b7f13ba79f1d2e529df64
Detanup01 <91248446+Detanup01@users.noreply.github.com>
2025-09-26 21:09:41 +0200

    add ini file r/w

---
+++ Shared/IniFile.cs
@@ -0,0 +1,141 @@
+﻿using IniParser;
+using IniParser.Model;
+
+namespace Shared;
+
+/// <summary>
+/// Interacting with the .ini files.
+/// </summary>
+public static class IniFile
+{
+    /// <summary>
+    /// Reading the <paramref name="section"/> and the <paramref name="key"/> from <paramref name="filename"/>
+    /// </summary>
+    /// <param name="filename">FileName to Read</param>
+    /// <param name="section">INI Section</param>
+    /// <param name="key">INI Key</param>
+    /// <returns>Readed value or <see cref="string.Empty"/></returns>
+    public static string Read(string filename, string section, string key)
+    {
+        FileIniDataParser parser = new();
+        IniData data = parser.ReadFile(filename, System.Text.Encoding.UTF8);
+        if (!data.Sections.ContainsSection(section))
+            return string.Empty;
+        if (!data[section].ContainsKey(key))
+            return string.Empty;
+        return data[section][key];
+    }
+
+    /// <summary>
+    /// Reading the <see cref="KeyData"/> in <paramref name="section"/> with the <paramref name="key"/> from <paramref name="filename"/>
+    /// </summary>
+    /// <param name="filename">FileName to Read</param>
+    /// <param name="section">INI Section</param>
+    /// <param name="key">INI Key</param>
+    /// <returns>Readed <see cref="KeyData"/> or <see langword="null"/></returns>
+    public static KeyData? GetKeyData(string filename, string section, string key)
+    {
+        FileIniDataParser parser = new();
+        IniData data = parser.ReadFile(filename, System.Text.Encoding.UTF8);
+        if (!data.Sections.ContainsSection(section))
+            return null;
+        if (!data[section].ContainsKey(key))
+            return null;
+        return data[section].GetKeyData(key);
+    }
+
+    /// <summary>
+    /// Reading a <typeparamref name="T"/> type in a <paramref name="section"/> and the <paramref name="key"/> from <paramref name="filename"/>
+    /// </summary>
+    /// <typeparam name="T">A Type that has <see cref="IParsable{TSelf}"/></typeparam>
+    /// <param name="filename">FileName to Read</param>
+    /// <param name="section">INI Section</param>
+    /// <param name="key">INI Key</param>
+    /// <returns>Readed value or default</returns>
+    public static T? Read<T>(string filename, string section, string key) where T : IParsable<T>
+    {
+        string readed = Read(filename, section, key);
+        if (string.IsNullOrEmpty(readed))
+            return default;
+        T.TryParse(readed, null, out T? value);
+        return value;
+    }
+
+    /// <summary>
+    /// Write a <paramref name="value"/> to the <paramref name="filename"/> with a Key of <paramref name="key"/> and a Section as <paramref name="section"/>
+    /// </summary>
+    /// <param name="filename">FileName to write to</param>
+    /// <param name="section">INI Section</param>
+    /// <param name="key">INI Key</param>
+    /// <param name="value">The Value</param>
+    public static void Write(string filename, string section, string key, string? value)
+    {
+        if (value == null)
+            return;
+        if (!File.Exists(filename))
+        {
+            string? path = Path.GetDirectoryName(filename);
+            if (!string.IsNullOrEmpty(path))
+            {
+                Directory.CreateDirectory(path);
+            }
+            File.WriteAllText(filename, ";TMP");
+        }
+        FileIniDataParser parser = new();
+        IniData data = parser.ReadFile(filename, System.Text.Encoding.UTF8);
+        data[section][key] = value;
+        parser.WriteFile(filename, data, System.Text.Encoding.UTF8);
+    }
+
+    /// <summary>
+    /// Write a <paramref name="keyData"/> to the <paramref name="filename"/> in a Section as <paramref name="section"/>
+    /// </summary>
+    /// <param name="filename">FileName to write to</param>
+    /// <param name="section">INI Section</param>
+    /// <param name="keyData">Key data</param>
+    public static void Write(string filename, string section, KeyData? keyData)
+    {
+        if (keyData == null)
+            return;
+        if (!File.Exists(filename))
+        {
+            string? path = Path.GetDirectoryName(filename);
+            if (!string.IsNullOrEmpty(path))
+            {
+                Directory.CreateDirectory(path);
+            }
+            File.WriteAllText(filename, ";TMP");
+        }
+        FileIniDataParser parser = new();
+        IniData data = parser.ReadFile(filename, System.Text.Encoding.UTF8);
+        data[section].SetKeyData(keyData);
+        parser.WriteFile(filename, data, System.Text.Encoding.UTF8);
+    }
+
+
+    /// <summary>
+    /// Checks if the <paramref name="section"/> and <paramref name="key"/> exists inside the <paramref name="filename"/>
+    /// </summary>
+    /// <param name="filename">FileName to read from</param>
+    /// <param name="section">INI Section</param>
+    /// <param name="key">INI Key</param>
+    /// <returns><see langword="true"/> if Exists <see langword="false"/> if not</returns>
+    public static bool Exists(string filename, string section, string key)
+    {
+        string readed = Read(filename, section, key);
+        return !string.IsNullOrEmpty(readed);
+    }
+
+    /// <summary>
+    /// Write a <paramref name="value"/> of Type <typeparamref name="T"/> to the <paramref name="filename"/> with a Key of <paramref name="key"/> and a Section as <paramref name="section"/>
+    /// </summary>
+    /// <typeparam name="T">A Type that has <see cref="IParsable{Self}"/></typeparam>
+    /// <param name="filename">FileName to write to</param>
+    /// <param name="section">INI Section</param>
+    /// <param name="key">INI Key</param>
+    /// <param name="value">The Value</param>
+    public static void Write<T>(string filename, string section, string key, T value) where T : IParsable<T>
+    {
+        Write(filename, section, key, value.ToString());
+    }
+}
